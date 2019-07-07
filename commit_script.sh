0bb69a16f8ad8c7cd25d247fb9fd2aefb5ece856
The-Noble-K <jarod.sneed@gmail.com>
2019-07-07 13:33:45 -0400

    post-mortem

--- commit_script.sh
+++ commit_script.sh
@@ -1,47 +1,52 @@
-for Y in {2015..2018} do
-     mkdir $Y_commits
-     cd $Y_commits
-     for M in {1..12}  do
-         mkdir $M_commits
-         cd $M_commits
-         for D in {1..31}  do
-             mkdir $D_commits
-             cd $D_commits
-             for i in {1..12}  do
-                 echo "$i on $M/$D/$Y" > commit.md
-                 export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
-                 export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
-                 git add commit.md -f
-                 git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
-             done
-         cd ../
-         done
-     cd ../
-     done
- cd ../
-done
-git push origin master
-for Y in {2015..2019} do
-    mkdir $Y_commits_2
-    cd $Y_commits_2
-    for M in {1..6} do
-        mkdir $M_commits_2
-        cd $M_commits_2
-        for D in {1..31} do
-            mkdir $D_commits_2
-            cd $D_commits_2
-            for i in {1..12} do
-                echo "$i_2 on $M/$D/$Y" > commit_2.md
-                export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
-                export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
-                git add commit_2.md -f
-                git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
+for Y in {2017..2019}
+do
+    if [$Y == 2019]
+    then
+        mkdir $Y && cd $Y
+        for M in {1..6}
+        do
+            mkdir $M && cd $M
+            for D in {1..31}
+            do
+                mkdir $D && cd $D
+                for i in {1..12}
+                do
+                    echo "$i on $M/$D/$Y" > commit_$i.md
+                    export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
+                    export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
+                    git add commit_$i.md -f
+                    git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
+                done
+            cd ../
             done
         cd ../
         done
-    cd ../
-    done
+    else
+        mkdir $Y && cd $Y
+        for M in {1..12}
+        do
+            mkdir $M && cd $M
+            for D in {1..31}
+            do
+                mkdir $D && cd $D
+                for i in {1..12}
+                do
+                    echo "$i on $M/$D/$Y" > commit_$i.md
+                    export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
+                    export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
+                    git add commit_$i.md -f
+                    git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
+                done
+            cd ../
+            done
+        cd ../
+        done
+    fi
 cd ../
 done
+git push origin master
+git rm -rf 20**
+git rm -rf 19**
+git commit -am "tidy up"
 git push origin master
 
